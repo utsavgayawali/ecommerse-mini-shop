429da765b2b556009b36daa40f477f4d68c979fd
Utsab Gayawali <utsavgayawali@gmail.com>
2025-07-16 16:54:48 +0545

    8th commit

--- home/views.py
+++ home/views.py
@@ -4,8 +4,8 @@ from product.models import Product
 
 
 def home(request):
-  product=Product.objects.all
-  print(f"[DEBUG] Product count in DB: {product}") 
+  product=Product.objects.all()
+  print(f"[DEBUG] Products: {list(product)}")
   context={'items':product}
   return render(request,'home/home.html',context)
 
