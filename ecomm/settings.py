6cc334d0e2a2d9be240a8d2ea03e29aca2edbecf
Utsab Gayawali <utsavgayawali@gmail.com>
2025-07-16 08:07:12 +0545

    3rd commit

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -150,6 +150,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'public','media')
 
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
